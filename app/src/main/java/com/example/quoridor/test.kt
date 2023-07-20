38cc770191d88c124f1d65c02732b3c5240a131d
mongky9103 <mongky9103@naver.com>
2023-07-21 00:22:29 +0900

    login & write database

--- app/src/main/java/com/example/quoridor/test.kt
+++ app/src/main/java/com/example/quoridor/test.kt
@@ -5,9 +5,11 @@ import androidx.activity.ComponentActivity
 import com.example.quoridor.databinding.ActivityTestBinding
 
 class test : ComponentActivity() {
+    val hello:String = "hello"
     override fun onCreate(savedInstanceState: Bundle?){
         val binding = ActivityTestBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
     }
 }
